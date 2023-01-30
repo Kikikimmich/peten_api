579048e1422e32931386634b0c17bee33bebb5f0
Kikikimmich <2647221298@qq.com>
2023-01-30 14:47:58 +0800

    api项目骨架

---
+++ src/main/java/com/kimmich/peten/PetenApiApplication.java
@@ -0,0 +1,13 @@
+package com.kimmich.peten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetenApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetenApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kimmich/peten/PetenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimmich.peten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
